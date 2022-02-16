db47b1e728ba1c7a776729abe888ff960b2199f6
Your Name <you@example.com>
2022-02-16 12:16:56 +0530

    2. About Section

---
+++ src/components/AboutSection.js
@@ -0,0 +1,29 @@
+import React from "react";
+import home1 from "../img/home1.png";
+
+const AboutSection = () => {
+    return(
+        <div>
+        <div className="description">
+            <div className="title">
+                <div className="hide">
+                    <h2>We work to make</h2>
+                </div>
+                <div className="hide">
+                    <h2>Your <span>dreams</span> come</h2>
+                </div>
+                <div className="hide">
+                    <h2>true.</h2>
+                </div>
+            </div>
+            <p>Contact us for any photography ideas that you have. We have amazing skills.</p>
+            <button>Contact US</button>
+        </div>
+        <div className="image">
+            <img src={home1} alt="guy with a camera" />
+        </div>
+    </div>
+    );
+};
+
+export default AboutSection;
---
+++ src/pages/AboutUs.js
@@ -0,0 +1,8 @@
+import React from "react";
+import AboutSection from "../components/AboutSection";
+
+const AboutUS = () => {
+    return <AboutSection/>;
+};
+
+export default AboutUS;
