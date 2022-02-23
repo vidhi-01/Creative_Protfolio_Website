bc1d8fa67a15f5f6071daf2f108ef2654f54a1a8
Your Name <you@example.com>
2022-02-23 22:11:33 +0530

    16. Our work page animation

--- src/animation.js
+++ src/animation.js
@@ -8,13 +8,12 @@ export const pageAnimation = {
     y: 0,
     transition: {
       staggerChildren: 0.25,
-      duration: 1,
+      duration: 0.5,
       when: 'beforeChildren',
     },
   },
   exit: {
     opacity: 0,
-    y: 300,
     transition: { 
         ease: 'easeOut', 
         duration: 0.5,
@@ -48,6 +47,38 @@ export const photoAnim = {
       ease: 'easeOut',
       duration: 1,
       delay: 0.5,
+    },
+  },
+};
+
+export const lineAnim = {
+  hidden: { width: '0%' },
+  show: {
+    width: '100%',
+    transition: { duration: 1 },
+  },
+};
+
+export const slider = {
+  hidden: { x: '-130%', skew: '45deg' },
+  show: {
+    x: '100%',
+    skew: '0deg',
+    transition: { type: 'tween', ease: 'easeOut', duration: 1 },
+  },
+};
+
+export const sliderContainer = {
+  hidden: {
+    opacity: 1,
+  },
+  show: {
+    opacity: 1,
+
+    transition: {
+      staggerChildren: 0.15,
+      ease: 'easeOut',
+      duration: 1,
     },
   },
 };
--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -5,23 +5,31 @@ import athelete from "../img/athlete-small.png";
 import theracer from "../img/theracer-small.png";
 import goodtimes from "../img/goodtimes-small.png";
 import { motion } from 'framer-motion';
-import { pageAnimation } from '../animation';
+import { pageAnimation, fade, photoAnim, lineAnim, slider, sliderContainer } from '../animation';
 
 const OurWork = () => {
     return( 
         <Work style={{ background: '#fff' }} exit= "exit" variants={pageAnimation} initial="hidden" animate="show">
+            <motion.div variants={sliderContainer}>
+            <Frame1 variants={slider}></Frame1>
+            <Frame2 variants={slider}></Frame2>
+            <Frame3 variants={slider}></Frame3>
+            <Frame4 variants={slider}></Frame4>
+            </motion.div>
             <Movie>
-                <h2>The Athelete</h2>
-                <div className="line"></div>
+                <motion.h2 variants={fade}>The Athelete</motion.h2>
+                <motion.div variants={lineAnim} className="line"></motion.div>
                 <Link to="/work/the-athlete">
-                    <img src={athelete} alt="athelete" />
+                    <Hide>
+                        <motion.img variants={photoAnim} src={athelete} alt="athelete" />
+                    </Hide>
                 </Link>
             </Movie>
             <Movie>
-                <h2>The Racer</h2>
-                <div className="line"></div>
+                <motion.h2 variants={fade}>The Racer</motion.h2>
+                <motion.div className="line"></motion.div>
                 <Link to="/work/the-racer">
-                    <img src={theracer} alt="theracer" />
+                    <motion.img variants={photoAnim} src={theracer} alt="theracer" />
                 </Link>
             </Movie>
             <Movie>
@@ -47,8 +55,8 @@ const Work = styled(motion.div)`
 const Movie = styled.div`
     padding-bottom: 10rem;
     .line{
-        height: 0%.5rem;
-        background: #cccccc;
+        height: 0.5rem;
+        background: #23d997;
         margin-bottom: 3rem;
     }
     img{
@@ -56,6 +64,29 @@ const Movie = styled.div`
         height: 70vh;
         object-fit: cover;
     }
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+`;
+
+const Frame1 = styled(motion.div)`
+  position: fixed;
+  left: 0;
+  top: 10%;
+  width: 100%;
+  height: 100vh;
+  background: #fffebf;
+  z-index: 2;
+`;
+const Frame2 = styled(Frame1)`
+  background: #ff8efb;
+`;
+const Frame3 = styled(Frame1)`
+  background: #8ed2ff;
+`;
+const Frame4 = styled(Frame1)`
+  background: #8effa0;
 `;
 
 export default OurWork;
